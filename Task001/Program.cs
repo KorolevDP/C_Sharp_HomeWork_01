759bd281bcdaa0cf601b39ff0123d1649b06b8ef
korolevdp <korolevdp-228@yandex.ru>
2023-02-15 19:36:31 +0400

    Added function in home work tasks

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,16 +1,26 @@
 ﻿//Напишите программу, которая на вход принимает два числа и выдает, какое число большеe, а какое меньшее.
 
-Console.Write("Enter first number  >");
-int number1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Enter second number >");
-int number2 = Convert.ToInt32(Console.ReadLine());
-
-if(number1 > number2)
+int Promt(string message)
 {
-    Console.Write($"max = {number1} \nmin = {number2}");    
+    Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+  
+int value1 = Promt("Enter first number");
+int value2 = Promt("Enter second number");
+
+int max = 0;
+int min = 0;
+
+if (value1 > value2)
+{
+    max = value1; 
+    min = value2;    
 }
 else
 {
-    Console.Write($"max = {number2} \nmin = {number1}"); 
+    max = value2; 
+    min = value1;    
 }
+
+Console.Write($"Max value >{max}\nMin value >{min}");
--- Task004/Program.cs
+++ Task004/Program.cs
@@ -3,10 +3,7 @@
 Console.Write("Enter your number >");
 int number = int.Parse(Console.ReadLine());
 
-for(int i = 2; i <= number; i++ )
-{
-    if(i % 2 ==0)
-    {
-        Console.Write($"{i}, ");
-    }
+for(int i = 2; i <= number; i+=2 )
+{        
+  Console.Write($"{i}, ");    
 }
