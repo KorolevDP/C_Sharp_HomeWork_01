005d09276a6cab279888d6b21001c76bde203b9e
korolevdp <korolevdp-228@yandex.ru>
2023-02-18 21:01:12 +0400

    Added tasks: Task009, Task010

---
+++ Task010/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int InputInt(string message)
+{
+    Console.Write($"{message}> ");
+    int value;
+    if(int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    Console.WriteLine("You enter not number");
+    Environment.Exit(1);
+    return 0;
+}
+
+int number = InputInt("Enter number");
+
+for(int i = 1; i <= number; i++)
+{
+  System.Console.Write( $"{Math.Pow(i,3)}, ");
+}
