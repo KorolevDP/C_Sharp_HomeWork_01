a571c44c9a697d95773d850fdacd52e78c12cecd
korolevdp <korolevdp-228@yandex.ru>
2023-02-21 19:10:52 +0400

    Added Task008

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -5,3 +5,40 @@
 // 12821 -> да
 // 23432 -> да
 
+int InputInt(string message)
+{
+    Console.Write($"{message}> ");
+    int value;
+    if (int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    Console.WriteLine("You enter not number");
+    Environment.Exit(1);
+    return 0;
+}
+
+bool checkPalindrome(int num)
+{
+    int number = num;
+    int rev = 0;
+
+    while (number > 0)
+    {
+        int takenDigit = number % 10;
+        rev = rev * 10 + takenDigit;
+        number /= 10;
+    }
+    return (num == rev);
+}
+
+int number = InputInt("Enter number");
+
+if (checkPalindrome(number))
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
