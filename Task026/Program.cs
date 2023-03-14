af932a43c3fe21c1863958fbf28fc27a47e0c424
korolevdp <korolevdp-228@yandex.ru>
2023-03-14 23:23:53 +0400

    Added tasks: Task025, Task026, Task027

---
+++ Task026/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int SumNumbersOfRange(int first, int second)
+{       
+    if (first > second) return 0;
+    return first + SumNumbersOfRange(first + 1, second);       
+}
+
+int num = 1, second = 16;
+Console.WriteLine();
+Console.Write($"M = {num}; N = {second} ---> {SumNumbersOfRange(num, second)}");
+Console.WriteLine("\n");
---
+++ Task027/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+
+// m = 3, n = 2 -> A(m,n) = 29
+
+int FunctionAckermann(int m, int n)
+{       
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return FunctionAckermann(m - 1, 1);
+    if (m > 0 && n > 0) return FunctionAckermann(m - 1, FunctionAckermann(m, n - 1));       
+    return FunctionAckermann(m,n);
+}
+
+int m = 3, n = 2;
+Console.WriteLine();
+Console.Write($"{FunctionAckermann(m,n)}");
+Console.WriteLine("\n");
